0e498c12e660f1531e827c558a9eaf5a6dab98cf
davidprager <david_prager@yahoo.com>
2021-12-04 19:38:15 -0500

    Initial commit

---
+++ processForm.js
@@ -0,0 +1,49 @@
+/* This file processes a reservation form */
+
+"use strict"
+
+// Define constants for each of the error messages
+
+
+
+function setupForm() {
+
+    // Define variables for all actionable elements in the form
+
+
+    // Set up event listeners for fields on form
+
+
+    // This function checks
+    function check() {
+
+    }
+}
+
+function validateForm() {
+    // Define variables for all actionanle elements in the form
+
+
+    // Set errors variable to false.  At the end of validation, errors will still be false if there are no errors
+    let errors = false;
+
+
+    // Check
+    if () {
+
+        errors = true;
+    }
+
+    // If there are errors, return false, which will prevent the form from being submitted to the server
+    // If there are no errors, display the success message
+    if (errors) {
+        return false;
+    } else {
+        responseDisplay.style.display = "block";
+        return true;
+    }
+}
+
+
+
+
